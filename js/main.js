ab3daa7408d18bb6dbc4cdb5dd4470f926bdee74
applebbox <leesbee@naver.com>
2021-08-09 10:28:46 +0900

    project
    delta

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+  $('.galleryList a').click(function(){
+
+    var imgSrc = $(this).find('img').attr('src');
+
+ 
+    $('#gallery figure img').attr('src', imgSrc );
+
+    return false;
+  });
+
+});
